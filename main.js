378cc40a0fb57e63048122a0467487920bf6c5c2
Megaju <megaju@kijewoku.fr>
2017-05-09 15:20:30 +0200

    base projet phaser

---
+++ main.js
@@ -0,0 +1,18 @@
+
+/**
+ * Created by megaju on 09/05/17.
+ */
+const main = {
+
+    preload() {
+        // Charger les fichiers nécessaires au jeu
+        game.load.image('player', 'assets/player.png');
+    },
+    create() {
+
+    },
+    update() {
+        // sur une base de 60fps
+    }
+
+}
