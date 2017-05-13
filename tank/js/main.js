a23f14466a6277a400c3bb312ad6647c10541964
Megaju <megaju@kijewoku.fr>
2017-05-13 13:08:45 +0200

    init added

--- tank/js/main.js
+++ tank/js/main.js
@@ -4,3 +4,22 @@
 
 const game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
 
+const main = {
+
+    init() {
+        game.renderer.renderSession.roundPixels = true;
+        game.world.setBounds(0, 0, 992, 480);
+        physics.startSystem(Phaser.Physics.ARCADE);
+        physics.arcade.gravity.y = 200;
+    },
+    preload() {
+
+    },
+    create() {
+
+    },
+    update() {
+
+    },
+
+};
